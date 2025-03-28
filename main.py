3565c1c2619d033b58d84c85a5ab810963405beb
ebuka-hub <albertebuka69@gmail.com>
2025-03-28 08:32:01 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+
+#creating canvas
+turtle.Screen().bgcolour("bule")
+
+Sc=turtle.Screen()
+sc.setup(600,400)
+
+turtle..title("welcome to Turtle world")
+#turtle object creation
+board=turtle.Turtle()
+
+#creatin
